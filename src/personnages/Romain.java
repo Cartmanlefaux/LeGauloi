c6079e6b2d8e856f42d25097c561a960a0185cf5
Cartmanlefaux <matteo.lafitte--carbonne@univ-tlse3.fr>
2024-09-27 12:35:53 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,13 +3,17 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
-	    if (force <= 0) {
-	    	throw new IllegalArgumentException("La force d'un Romain doit être positive.");
-	    }
+		if (force <= 0) {
+			throw new IllegalArgumentException("La force d'un Romain doit être positive.");
+		}
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
+		this.nbEquipement = 0;
 	}
 
 	public String getNom() {
@@ -27,17 +31,17 @@ public class Romain {
 
 	public void recevoirCoup(int forceCoup) {
 		if (force <= 0) {
-            throw new IllegalStateException("La force d'un Romain doit être positive pour recevoir un coup.");
-        }
+			throw new IllegalStateException("La force d'un Romain doit être positive pour recevoir un coup.");
+		}
 
-        int forceInitiale = force;
+		int forceInitiale = force;
 
-        force -= forceCoup;
+		force -= forceCoup;
 
-        if (force >= forceInitiale) {
-            throw new IllegalStateException("La force d'un Romain doit avoir diminué après avoir reçu un coup.");
-        }
-        
+		if (force >= forceInitiale) {
+			throw new IllegalStateException("La force d'un Romain doit avoir diminué après avoir reçu un coup.");
+		}
+
 		if (force > 0) {
 			parler("Aie");
 		} else {
@@ -46,13 +50,43 @@ public class Romain {
 
 	}
 
+	public void ajouterEquipement(Equipement equipement) {
+		if (nbEquipement < equipements.length) {
+			equipements[nbEquipement] = equipement;
+			nbEquipement++;
+		}
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+			} else {
+				ajouterEquipement(equipement);
+				System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+			}
+			break;
+		case 0:
+			ajouterEquipement(equipement);
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+			break;
+		default:
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
-		Romain cesar = new Romain("César", 10);
-		Romain minus = new Romain("Minus", 6);
-		System.out.println(cesar.getNom());
-		cesar.parler("Je suis le grand César");
-		cesar.recevoirCoup(3);
-		System.out.println("La force de César est de " + cesar.force);
+
+		Romain minus = new Romain("Minus", 10);
+
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 
 }
